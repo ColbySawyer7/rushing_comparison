2f3389d29d35be4dafbf5fabd7e2e113656d48a2
ColbySawyer7 <sawyerc17@students.ecu.edu>
2021-09-15 09:23:39 -0400

    Added Data Visualization for current rushing totals

--- app.py
+++ app.py
@@ -1,16 +1,56 @@
-import nfl_data_py as nfl
+import nfl_data_py
 from pywebio.input import *
 from pywebio import *
 from pywebio.output import *
 import pywebio
+import base64
 import pandas as pd
+import plotly.express as px
+import numpy as np
+from bs4 import BeautifulSoup
+
+players = [{
+ 'name': 'Myles Gaskin',
+ 'rushing_yds': 49,
+ 'rushing_att': 9
+}, {
+ 'name': 'Ezekiel Elliot',
+ 'rushing_yds': 33,
+ 'rushing_att': 11
+}]
+
 def app():
-    session.set_env(title="Gaskins vs Zeke")
-    put_markdown("<h2>Welcome, Please enjoy the beautiful data visualization below</h2>")
-    put_column([
-        print(zeke_headshot),
-        print(gaskins_headshot),
-    ])
+    pywebio.session.set_env(title="Gaskins vs Zeke")
+    pywebio.output.put_markdown("<h2>Welcome, please enjoy the beautiful data visualization below</h2>")
+
+    html = printBarChart().to_html(include_plotlyjs="require", full_html=False)
+    pywebio.output.put_html(html)
+
+    pywebio.output.put_markdown("Current Leader")
+    data_uri = base64.b64encode(open('gaskin.png', 'rb').read()).decode('utf-8')
+    img_tag = '<img src="data:image/png;base64,{0}">'.format(data_uri)
+    pywebio.output.put_html(img_tag)
+
+    #pywebio.output.put_html(BeautifulSoup(open('pay.html'), features='html.parser').get_text())
+    #NFL-Data Blog
+    #df = pd.read_csv('https://raw.githubusercontent.com/ArrowheadAnalytics/next-gen-scrapy-2.0/master/pass_and_game_data.csv', index_col=0)
+    #Filter year
+    #df_2020 = df.loc[df['season'] == 2020]
+    #print(df_2020)
+    #Get Player Stats for 2020
+
+def printBarChart():
+    data = pd.DataFrame(players)
+    fig = px.bar(players, x='name', y='rushing_yds', color='name', title = "Total Rushing Yards 2021")
+    return fig
+
+def getRushingStats(name) -> dict:
+    for player in players:
+        name = player.get('name', None)
+        rushing_yds = player.get('rushing_yds', None)
+        rushing_att = player.get('rushing_att', None)
+        if name and rushing_yds and rushing_att:
+            print(name + ' had a yd per carry average of ' + str(rushing_yds/rushing_att))
 
 if __name__ == '__main__':
     start_server(app, port=8081, debug=True)
