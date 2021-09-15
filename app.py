8dd8f8156ffa5d826bd352b956f5129144f917a1
ColbySawyer7 <sawyerc17@students.ecu.edu>
2021-09-15 13:57:23 -0400

    Updated data from static to dynamic

--- app.py
+++ app.py
@@ -10,44 +10,46 @@ import numpy as np
 from bs4 import BeautifulSoup
 import argparse
 
-players = [{
- 'name': 'Myles Gaskin',
- 'rushing_yds': 49,
- 'rushing_att': 9
-}, {
- 'name': 'Ezekiel Elliot',
- 'rushing_yds': 33,
- 'rushing_att': 11
-}]
-
 def app():
     set_env(title="Gaskins vs Zeke")
     put_markdown("<h2>Welcome, please enjoy the beautiful data visualization below</h2>")
 
-    html = printBarChart().to_html(include_plotlyjs="require", full_html=False)
+    # Retrieve Year Data
+    df_2021 = nfl.import_weekly_data([2021], downcast=True)
+    #print(df_2021)
+
+    #Fetch Gaskins and Zeke Data
+    gaskin = getPlayerData('M.Gaskin',df_2021)
+    #print(gaskin)
+    zeke = getPlayerData('E.Elliott', df_2021)
+    #print(zeke)
+    total_stats =  pd.concat([gaskin, zeke], axis=0)
+
+    html = printBarChart(total_stats).to_html(include_plotlyjs="require", full_html=False)
     put_html(html)
 
     put_markdown("Current Leader")
-    data_uri = base64.b64encode(open('gaskin.png', 'rb').read()).decode('utf-8')
+    put_html(getCurrentLeader(total_stats))
+
+def getCurrentLeader(data):
+    gaskin_yds = data['rushing_yards'].loc[data['player_name'] == 'M.Gaskin'].values[0]
+    #print(gaskin_yds)
+    zeke_yds = data['rushing_yards'].loc[data['player_name'] == 'E.Elliott'].values[0]
+    #print(zeke_yds)
+    if gaskin_yds > zeke_yds:
+        data_uri = base64.b64encode(open('gaskin.png', 'rb').read()).decode('utf-8')
+    else:
+        data_uri = base64.b64encode(open('zeke.png', 'rb').read()).decode('utf-8')
+
     img_tag = '<img src="data:image/png;base64,{0}">'.format(data_uri)
-    put_html(img_tag)
+    return img_tag
 
-    df_2021 = nfl.import_weekly_data([2021], downcast=True)
-    print(df_2021)
+def getPlayerData(name, year_data):
+    return year_data.loc[year_data['player_name'] == name]
 
-
-def printBarChart():
-    data = pd.DataFrame(players)
-    fig = px.bar(players, x='name', y='rushing_yds', color='name', title = "Total Rushing Yards 2021")
+def printBarChart(data):
+    fig = px.bar(data, x='player_name', y='rushing_yards', color='player_name', title = "Total Rushing Yards 2021")
     return fig
-
-def getRushingStats(name) -> dict:
-    for player in players:
-        name = player.get('name', None)
-        rushing_yds = player.get('rushing_yds', None)
-        rushing_att = player.get('rushing_att', None)
-        if name and rushing_yds and rushing_att:
-            print(name + ' had a yd per carry average of ' + str(rushing_yds/rushing_att))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
