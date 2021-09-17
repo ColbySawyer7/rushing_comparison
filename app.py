0e6ac25a441b21c63d117001df654bf8c91c0a69
ColbySawyer7 <sawyerc17@students.ecu.edu>
2021-09-17 08:59:09 -0400

    Added Weekly data select

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
+from numpy.lib.function_base import median
 import nfl_data_py as nfl
-from pywebio.input import *
+from pywebio.input import select
 from pywebio.session import set_env
-from pywebio.output import put_markdown, put_html, put_column, put_row
+from pywebio.output import put_markdown, put_html, put_column, put_row, popup
 from pywebio.platform.tornado_http import start_server
 import base64
 import pandas as pd
@@ -12,7 +13,7 @@ import argparse
 
 def app():
     set_env(title="Gaskin vs Zeke")
-    put_markdown("<h2>Welcome, please enjoy the beautiful data visualization below</h2>")
+    put_markdown("<h2>NFL Data Comparison of M.Gaskin and E.Elliott</h2>")
 
     # Retrieve Year Data
     df_2021 = nfl.import_weekly_data([2021], downcast=True)
@@ -34,9 +35,27 @@ def app():
     put_html(full_season_bar)
     put_html(full_season_line)
 
-    put_markdown("Current Leader")
+    put_markdown("<h3>Current Leader<h3>")
     put_html(getCurrentLeader(total_stats))
-    
+
+    available_weeks = total_stats['week'].value_counts(normalize=True)
+    #print(available_weeks)
+
+
+    selected_week = select("Select Week for week specific data", options=available_weeks)
+    if(selected_week is not None):
+        selected_week_stats = total_stats.loc[total_stats['week'] == selected_week]
+        #Week Bar Graph (Totals)
+        title = "Week " + str(selected_week) + " Rushing Totals"
+        week_bar = printBarChart(selected_week_stats,title).to_html(include_plotlyjs="require", full_html=False)
+        popup(title,[
+            put_html(week_bar),
+            put_markdown("Week Leader"),
+            put_html(getCurrentLeader(selected_week_stats))
+        ], size='large')
+        
+
+
 def getCurrentLeader(data):
     gaskin_yds = data['rushing_yards'].loc[data['player_name'] == 'M.Gaskin'].values[0]
     #print(gaskin_yds)
@@ -53,8 +72,8 @@ def getCurrentLeader(data):
 def getPlayerData(name, year_data):
     return year_data.loc[year_data['player_name'] == name]
 
-def printBarChart(data):
-    fig = px.bar(data, x='player_name', y='rushing_yards', color='player_name', title = "Total Rushing Yards 2021")
+def printBarChart(data, title="Total Rushing Yards 2021"):
+    fig = px.bar(data, x='player_name', y='rushing_yards', color='player_name', title = title)
     return fig
 
 def printLineChart(data):
